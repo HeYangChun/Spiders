c5677f8b9370b40a7ffeb87ad8bb06e1e9159450
heyc <yangchun_he@qq.com>
2020-01-06 21:54:57 +0800

    start crawls

--- tools/startcrawl.sh
+++ tools/startcrawl.sh
@@ -2,12 +2,22 @@
 logfile=/home/andy/workspace/crontask.log
 
 cnt=$(ps -aux |grep scrapy|wc -l)
-echo $cnt >> $logfile
-if [ $cnt > 1 ];then
-    echo "scrapy is running,not need to be started again" >>${logfile} 
+
+if [ $cnt -gt 1 ];then
+    echo "$(date)    scrapy is running,not need to be started again" >>${logfile} 
     exit
 fi
 
-echo "starting scrapy...">>${logfile}
+echo "$(date)    starting scrapy...">>${logfile}
 cd /home/andy/workspace/crawler
+
+echo "$(date)    start crawl hslt..." >>${logfile}
+scrapy crawl hslt
+
+echo "$(date)    start crawl mllt..." >>${logfile}
+scrapy crawl mllt
+
+echo "$(date)    start crawl pacific..." >>${logfile}
 scrapy crawl tpysylt
+
+echo "$(date)    completed." >>${logfile}
